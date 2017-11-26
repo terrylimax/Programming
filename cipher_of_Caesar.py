b0986d79279a5acc0c7d84fc4bdae83dd638028d
terrylimax <dimon19_11@mail.ru>
2017-11-26 17:31:59 +0300

    decript_Caesar

--- cipher_of_Caesar.py
+++ cipher_of_Caesar.py
@@ -9,3 +9,15 @@ def encrypt_caesar(plaintext, shift):
     return ciphertext
 encrypt_caesar("PYTHON",29)
 encrypt_caesar("python",29)
+
+def decrypt_caesar(ciphertext, shift):
+    plaintext = ""
+    for symbol in ciphertext:
+        if ord(symbol.upper()) - shift%26 < ord('A'):
+            plaintext += chr(ord(symbol) - shift%26 + 26)
+        else:
+            plaintext += chr(ord(symbol) - shift%26)
+    print(plaintext)
+    return plaintext
+decrypt_caesar("SBWKRQ",29)
+decrypt_caesar("sbwkrq",29)
