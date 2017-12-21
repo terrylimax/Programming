7f7dfe62dadeb4b39869aed6de0d874c082d8b30
terrylimax <33511719+terrylimax@users.noreply.github.com>
2017-12-21 22:47:18 +0300

    Update cipher_of_Vigenere.py

--- cipher_of_Vigenere.py
+++ cipher_of_Vigenere.py
@@ -18,3 +18,23 @@ encrypt_vigenere("PYTHON", "A")
 encrypt_vigenere("python", "a")
 encrypt_vigenere("Python", "A")
 encrypt_vigenere("ATTACKATDAWN", "LEMON")
+
+def decrypt_vigenere(ciphertext, keyword):
+    plaintext = ""
+    index = 0
+    keyindex = 0
+    while index in range(len(ciphertext)):
+        if ord(ciphertext[index].upper()) - ord(keyword[keyindex].upper()) + 65 < ord('A'):
+            plaintext += chr(ord(ciphertext[index])- ord(keyword[keyindex].upper()) + 65 + 26)
+        else:
+            plaintext += chr(ord(ciphertext[index])- ord(keyword[keyindex].upper()) + 65)
+        if keyindex == len(keyword) - 1:
+            keyindex = 0
+        else:
+            keyindex += 1
+        index += 1
+    print(plaintext)
+    return plaintext
+decrypt_vigenere("PYTHON", "A")
+decrypt_vigenere("python", "a")
+decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
